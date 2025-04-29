edb63f640427efa4347fd8d2d4cfacae6c473b8c
VictorVangeli <whgaleon1@yandex.ru>
2025-04-29 19:03:16 +0300

    feat: добавлена главная страница и базовые шаблоны с версткой

--- page_analyzer/core/app.py
+++ page_analyzer/core/app.py
@@ -5,7 +5,7 @@ from page_analyzer.domain.pa_routes import app_route
 
 
 def run_app():
-    app = Flask(__name__, template_folder=get_settings().TEMPLATES_DIR)
+    app = Flask(__name__, template_folder=get_settings().TEMPLATES_DIR, static_folder=get_settings().STATIC_DIR)
 
     app.config['SECRET_KEY'] = get_settings().SECRET_KEY
     app.config['TIMEZONE'] = get_settings().TIME_ZONE
