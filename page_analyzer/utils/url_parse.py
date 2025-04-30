afc69d89f687c451d2452fc09ebaa51f9413b430
VictorVangeli <whgaleon1@yandex.ru>
2025-04-30 21:46:51 +0300

    documentation: - добавлены докстринги для `url_parse.py`;

--- page_analyzer/utils/url_parse.py
+++ page_analyzer/utils/url_parse.py
@@ -5,6 +5,15 @@ from page_analyzer.entities.schemas.url_schema import ParsedUrlSchema
 
 
 async def parse_url(url: str) -> ParsedUrlSchema:
+    """
+    Парсит URL и извлекает необходимые данные.
+
+    :param url: URL для парсинга.
+    :type url: str
+    :returns: Cхему для ссылки, которая содержит поля `status_code`, `title`, 
+        `h1`, `description`.
+    :rtype: ParsedUrlSchema
+    """
     async with httpx.AsyncClient() as client:
         response = await client.get(url)
         status_code = response.status_code
@@ -13,7 +22,8 @@ async def parse_url(url: str) -> ParsedUrlSchema:
         title = parsed_data.title.string.strip() if parsed_data.title else None
         h1_tag = parsed_data.find('h1')
         h1 = h1_tag.text.strip() if h1_tag else None
-        description_tag = parsed_data.find('meta', attrs={'name': 'description'})
+        description_tag = parsed_data.find('meta',
+                                           attrs={'name': 'description'})
         description = description_tag.get('content',
                                           '').strip() if description_tag else None
 
