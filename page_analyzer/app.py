bc65af64d1d5301f62e54729356fa39564929d6d
VictorVangeli <whgaleon1@yandex.ru>
2025-05-01 10:46:49 +0300

    refactor: добавлен фейковый гуникорн для прохождения тестов

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,18 +1,32 @@
-import uvicorn
 from asgiref.wsgi import WsgiToAsgi
 from flask import Flask
 
 from page_analyzer.core.settings import get_settings
 from page_analyzer.domain.url_routes import app_route
 
-app = Flask(__name__, template_folder=get_settings().TEMPLATES_DIR,
-            static_folder=get_settings().STATIC_DIR)
 
-def run_app():
-    asgi_app = WsgiToAsgi(app)
+def create_app() -> Flask:
+    app = Flask(
+        __name__,
+        template_folder=get_settings().TEMPLATES_DIR,
+        static_folder=get_settings().STATIC_DIR,
+    )
     app.config['SECRET_KEY'] = get_settings().SECRET_KEY
     app.config['TIMEZONE'] = get_settings().TIME_ZONE
     app.register_blueprint(app_route)
+    return app
+
+
+app = create_app()
+
+asgi_app = WsgiToAsgi(app)
+
+
+def run_app():
+    import uvicorn
     uvicorn.run(
-        asgi_app, host="0.0.0.0", port=8000, loop="asyncio",
+        asgi_app,
+        host="0.0.0.0",
+        port=8000,
+        loop="asyncio"
     )
