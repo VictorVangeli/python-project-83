228058cbf2e4cb54d88a816b4f6f497c6d7bf109
VictorVangeli <whgaleon1@yandex.ru>
2025-05-26 17:23:09 +0300

    refactor: миграции применяются при запуске приложения

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,14 +1,38 @@
+from alembic import command
+from alembic.config import Config
+from alembic.runtime.migration import MigrationContext
+from alembic.script import ScriptDirectory
 from flask import Flask
+from sqlalchemy import create_engine
 
 from page_analyzer.core.settings import get_settings
 from page_analyzer.domain.url_routes import app_route
 
 
+def run_migrations_if_needed():
+    alembic_cfg = Config("alembic.ini")
+    engine = create_engine(get_settings().DATABASE_URL)
+    with engine.connect() as conn:
+        current_rev = MigrationContext.configure(conn).get_current_revision()
+        script = ScriptDirectory.from_config(alembic_cfg)
+        head_rev = script.get_current_head()
+        if current_rev != head_rev:
+            print(f"Upgrading DB from {current_rev} → {head_rev}")
+            alembic_cfg.attributes["connection"] = conn
+            command.upgrade(alembic_cfg, "head")
+        else:
+            print("DB already up-to-date.")
+
+
 def run_app():
-    app = Flask(__name__, template_folder=get_settings().TEMPLATES_DIR,
-                static_folder=get_settings().STATIC_DIR)
-    app.config['SECRET_KEY'] = get_settings().SECRET_KEY
-    app.config['TIMEZONE'] = get_settings().TIME_ZONE
+    app = Flask(
+        __name__,
+        template_folder=get_settings().TEMPLATES_DIR,
+        static_folder=get_settings().STATIC_DIR,
+    )
+    app.config["SECRET_KEY"] = get_settings().SECRET_KEY
+    app.config["TIMEZONE"] = get_settings().TIME_ZONE
+    run_migrations_if_needed()
     app.register_blueprint(app_route)
     # ДЛЯ ЗАПУСКА ЮВИКОРНА
     # asgi_app = WsgiToAsgi(app)
